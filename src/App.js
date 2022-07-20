28a36be62e32a7070270b2ea90cff3d2f65a14bf
TeNeBiT-eng <ademolababalola2003@gmail.com>
2022-07-20 16:04:10 +0100

    assets and layout

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          <i class="fa-solid fa-grid"></i>
         </p>
         <a
           className="App-link"

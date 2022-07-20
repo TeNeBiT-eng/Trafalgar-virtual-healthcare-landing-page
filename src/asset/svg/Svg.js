820306f8f8f77297cc9bafdc954bf14132f42373
TeNeBiT-eng <ademolababalola2003@gmail.com>
2022-07-20 20:54:09 +0100

    Svg import

---
+++ src/asset/svg/Svg.js
@@ -0,0 +1,34 @@
+import Arrow from "./arrow.svg";
+import Bgelement from "./bgelementtralgar.svg";
+import Downloadarrow from "./downloadarrow.svg";
+import Invertedbgelement from "./invertedbgelement.svg";
+import Trafalgar_illustrationsec02 from "./trafalgar-illustrationsec02.svg";
+import Trafalgarconsultation from "./trafalgarconsultation.svg";
+import Trafalgardetailnote from "./trafalgardetailnote.svg";
+import Trafalgarelement from "./trafalgarelement.svg";
+import Trafalgarfooterlogo from "./trafalgarfooterlogo.svg";
+import Trafalgarheaderillustration from "./trafalgarheaderillustration.svg";
+import Trafalgarillustrationsec03 from "./trafalgarillustrationsec03.svg";
+import Trafalgarlogo from "./trafalgarlogo.svg";
+import Trafalgaronlinepharmarmacy from "./trafalgaronlinepharmarmacy.svg";
+import Trafalgarsearch from "./trafalgarsearch.svg";
+import Trafalgartracking from "./trafalgartracking.svg";
+
+const SVG = {
+  Arrow,
+  Bgelement,
+  Downloadarrow,
+  Invertedbgelement,
+  Trafalgar_illustrationsec02,
+  Trafalgarconsultation,
+  Trafalgardetailnote,
+  Trafalgarelement,
+  Trafalgarfooterlogo,
+  Trafalgarheaderillustration,
+  Trafalgarillustrationsec03,
+  Trafalgarlogo,
+  Trafalgaronlinepharmarmacy,
+  Trafalgarsearch,
+  Trafalgartracking,
+};
+export default SVG;
